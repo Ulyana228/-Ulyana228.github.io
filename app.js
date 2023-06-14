17c0286b215f6f02f5c83e000fc414625a440149
Ulyana228 <90844953+Ulyana228@users.noreply.github.com>
2023-06-15 01:13:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = "#2cab37";
 
 order.addEventListener("click", () => {
             document.getElementById("main").style.display = "none";
-            document.getElementById("form").style.display = "block";
+            document.getElementById("container").style.display = "block";
             document.getElementById("user_name").value = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
         });
 
